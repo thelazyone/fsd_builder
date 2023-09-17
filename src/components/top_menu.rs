bb06d833dbb499297c70423a181d38dd551f21c2
thelazyone <giacomo.pantalone@gmail.com>
2023-09-17 16:50:39 +0530

    removed some testing lines

--- src/components/top_menu.rs
+++ src/components/top_menu.rs
@@ -16,7 +16,7 @@ impl Component for TopMenu {
 
     fn view(&self, _: &Context<Self>) -> Html {
         html! {
-            <div class="top-menu" style="background-color: red;">
+            <div class="top-menu">
                 <span class="title">{"Your Game Title"}</span>
                 <button>{"Clear Roster"}</button>
                 <button>{"Load Roster"}</button>
